526976bfbbf400295c03a8ab1f727375812ae6ad
Muhammad Rajab <ichijo_rajab@hotmail.com>
2019-09-30 13:57:57 +0700

    Initial Commit

---
+++ BelajarSave&LoadCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BelajarSave&LoadCoreData
+//
+//  Created by Muhammad Rajab Priharsanto on 30/09/19.
+//  Copyright © 2019 Muhammad Rajab Priharsanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
