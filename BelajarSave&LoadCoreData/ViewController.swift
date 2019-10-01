3ce06be61c506e53d0809a940d68711975cda5fd
Muhammad Rajab <ichijo_rajab@hotmail.com>
2019-10-01 17:06:24 +0700

    Baru bisa masukin (save) data

--- BelajarSave&LoadCoreData/ViewController.swift
+++ BelajarSave&LoadCoreData/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var barangPertamaTextfield: UITextField!
+    @IBOutlet weak var barangKeduaTextfield: UITextField!
+    @IBOutlet weak var hargaBarangPertamaTextfield: UITextField!
+    @IBOutlet weak var hargaBarangKeduaTextfield: UITextField!
+    @IBOutlet weak var statusSaveLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func saveAll()
+    {
+        let barangPertama = barangPertamaTextfield.text
+        let barangKedua = barangKeduaTextfield.text
+        let hargaBarangPertama = Int(hargaBarangPertamaTextfield.text!)
+        let hargaBarangKedua = Int(hargaBarangKeduaTextfield.text!)
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate
+        else
+        {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Barang", in: managedContext)!
+        let barangKeCoreData = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        barangKeCoreData.setValue(barangPertama, forKey: "barangPertama")
+        barangKeCoreData.setValue(barangKedua, forKey: "barangKedua")
+        barangKeCoreData.setValue(hargaBarangPertama, forKey: "hargaBarangPertama")
+        barangKeCoreData.setValue(hargaBarangKedua, forKey: "hargaBarangKedua")
+        
+        do {
+            try managedContext.save()
+            print("Core Data Masuk")
+            statusSaveLabel.text = "Core Data Masuk"
+        }
+        catch let error as NSError
+        {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    @IBAction func saveButton(_ sender: UIButton)
+    {
+        saveAll()
+    }
+    
 
 }
 
